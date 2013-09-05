5f431b44a871c572cdf942cf1ac3e523efde5065
Lukas Ruge <ruge@itm.uni-luebeck.de>
2013-09-05 12:35:27 +0200

    Update

--- src/org/ambientdynamix/contextplugins/hue/PluginFactory.java
+++ src/org/ambientdynamix/contextplugins/hue/PluginFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) the Dynamix Framework Project
+ * Copyright (C) Institute of Telematics, Lukas Ruge
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
